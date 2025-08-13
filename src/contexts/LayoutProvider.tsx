6afa61b5a1214b6add4b9dfaca59eec62c1effc9
Công <yeusangtao96@gmail.com>
2025-08-13 15:39:49 +0700

    config react-hot-toast

--- src/contexts/LayoutProvider.tsx
+++ src/contexts/LayoutProvider.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { ThemeProvider } from 'next-themes';
 import { AnimatePresence } from 'framer-motion';
+import { Toaster } from 'react-hot-toast';
 import { usePathname } from '@/i18n/routing';
 
 function LayoutProvider({ children }: { children: Readonly<React.ReactNode> }) {
@@ -10,6 +11,21 @@ function LayoutProvider({ children }: { children: Readonly<React.ReactNode> }) {
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem storageKey="theme" disableTransitionOnChange>
       <AnimatePresence mode="wait">
         <div key={pathname}>{children}</div>
+        <Toaster
+          gutter={8}
+          position="top-center"
+          reverseOrder={false}
+          toastOptions={{
+            duration: 3000,
+            removeDelay: 1000,
+            // style: {
+            //   borderRadius: '12px',
+            //   background: 'var(--white)',
+            //   color: 'var(--coffee-color-v2)',
+            //   border: '1px solid var(--primary-bg)',
+            // },
+          }}
+        />
       </AnimatePresence>
     </ThemeProvider>
   );
